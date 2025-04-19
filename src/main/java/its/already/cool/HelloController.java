328750562d62275a639bca2d1f9318349cfefe31
Игорь <gariksyr@gmail.com>
2025-04-20 01:40:11 +0300

    все еще не так

--- src/main/java/its/already/cool/HelloController.java
+++ src/main/java/its/already/cool/HelloController.java
@@ -1,4 +1,12 @@
 package its.already.cool;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello_world";
+    }
 }
