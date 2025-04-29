a31ede43eff02a40d9135c93b5a3d5be1da0cc57
Игорь <gariksyr@gmail.com>
2025-04-29 17:48:40 +0300

    все работает(из-за thymeleaf spring6 а не spring5)

--- src/main/java/its/already/cool/HelloController.java
+++ src/main/java/its/already/cool/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello-world")
+    @GetMapping("/porno")
     public String sayHello(){
         return "hello_world";
     }
