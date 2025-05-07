9565e93a02d21e8869cdb75134375cd115cca26f
Игорь <gariksyr@gmail.com>
2025-05-07 19:59:09 +0300

    tipa calcUlator

--- src/main/java/its/already/cool/controllers/FirstController.java
+++ src/main/java/its/already/cool/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package its.already.cool.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,12 +10,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname){
-        System.out.println(name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname, Model model){
+        //System.out.println(name + " " + surname);
+        model.addAttribute("message","Hello " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodbyePage(){
         return "first/goodbye";
+    }
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") int a, @RequestParam("b") int b, @RequestParam("action") String action, Model model){
+        switch (action){
+            case "multiplication":
+                model.addAttribute("result",a * b);
+                break;
+            case "addition":
+                model.addAttribute("result",a + b);
+                break;
+            case "subtraction":
+                model.addAttribute("result",a - b);
+                break;
+            case "division":
+                model.addAttribute("result",a / (double)b);
+        }
+        return "first/calculator";
     }
 }
